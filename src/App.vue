6beaecfa25fca7ede25ba28777d126d766c7be2a
Z <nik_Dc@mail.ru>
2020-07-21 19:38:03 +0300

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <div id="app">
+    <the-header />
+    <router-view />
+  </div>
+</template>
+<script lang="ts">
+  import {Component, Vue} from 'vue-property-decorator';
+  import TheHeader from "@/components/TheHeader.vue";
+
+  @Component({
+    components: {
+      TheHeader
+    }
+  })
+  export default class Authorization extends Vue {
+
+  }
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+}
+
+#nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+</style>
---
+++ vue.config.js
@@ -0,0 +1,36 @@
+const path = require('path');
+
+
+
+module.exports = {
+  productionSourceMap: process.env.NODE_ENV !== 'production',
+  css: {
+    loaderOptions: {
+      sass: {
+        prependData: `
+          @import "./src/assets/styles/main.scss";
+        `,
+      },
+    },
+  },
+  chainWebpack: (config) => {
+    config.module
+      .rule('svg')
+      .exclude.add(resolve('src/assets/symbols'))
+      .end();
+    config.module
+      .rule('icons')
+      .test(/\.svg$/)
+      .include.add(resolve('src/assets/symbols'))
+      .end()
+      .use('svg-sprite-loader')
+      .loader('svg-sprite-loader')
+      .options({
+        symbolId: filePath => path.basename(filePath, '.svg'),
+      });
+  },
+};
+
+function resolve(dir) {
+  return path.join(__dirname, './', dir);
+}
