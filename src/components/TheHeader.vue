6fe34b2d0f26049918d03c64eb49b9c8a26a0aa1
Z <nik_Dc@mail.ru>
2020-07-21 19:38:39 +0300

    header

---
+++ src/components/TheHeader.vue
@@ -0,0 +1,22 @@
+<template>
+  <header class="the-header">
+
+  </header>
+</template>
+<script lang="ts">
+  import {Component, Vue} from 'vue-property-decorator';
+
+  @Component
+  export default class Authorization extends Vue {
+
+  }
+</script>
+<style lang="scss">
+  .the-header {
+    background-color: $primary-blue;
+    width: 100%;
+    height: 100px;
+    position: absolute;
+    z-index: -1;
+  }
+</style>
